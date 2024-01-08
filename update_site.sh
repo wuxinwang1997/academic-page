0eebbd587c1502abebb4ae50c538eda6f9af3818
心吾人言 <wuxinwang@nudt.edu.cn>
2024-01-08 20:05:03 +0800

    Initial commit

--- update_site.sh
+++ update_site.sh
@@ -1,10 +1,10 @@
 git add .
 git commit -m "Initial commit"
-git push -u origin main
+git push -u origin main --force
 
 hugo
 cd public
 git add .
 git commit -m "Build website"
-git push origin master
+git push origin master --force
 cd ..
